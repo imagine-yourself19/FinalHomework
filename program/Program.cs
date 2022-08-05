f2346c20df6cfad60d8616def220cf6b841c3b1b
Lena <imagine.youeself19@gmail.com>
2022-08-05 14:07:30 +0300

    add creation of two arrays

---
+++ program/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Напишите кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array1 = new string[n];
+Console.WriteLine("Напишите элементы массива: ");
+for (int i = 0; i < array1.Length; i++)
+{
+    array1[i] = Convert.ToString(Console.ReadLine());
+}
+string[] array2 = new string[array1.Length];
